7b95f88a0e2f2d6ebc437b3000a0547c7a6a255a
patriciaCastillo <patrycast81@gmail.com>
2025-11-07 21:19:12 -0300

    agregando filtros

--- filters.js
+++ filters.js
@@ -1,14 +1,23 @@
-  // filtros
+
 
    const filterTechnology= document.getElementById("filter-technology")
    const filterLocation= document.getElementById("filter-location");
    const filterExperienceLevel= document.getElementById("filter-experience-level")
   
    
-   // filterTechnology.addEventListener("change", function(){
-   //    console.log("tecnologia seleccionada: ", filterTechnology.value)
-   // })
+   filterTechnology.addEventListener("change", function(){
+      const technology= document.querySelectorAll(".job-listing-card"); 
+      const technologySelectedValue=filterTechnology.value;console.log
 
+      technology.forEach(tech=>{
+         const tecnologia= tech.getAttribute("data-technology"); (tecnologia)
+         const isShown= technologySelectedValue === "" || technologySelectedValue === tecnologia;
+         tech.classList.toggle("is-hidden", !isShown);
+      });
+   });
+
+
+  // filtros ubicacion
 
     filterLocation.addEventListener("change", function(){
        const jobs= document.querySelectorAll(".job-listing-card");
@@ -31,3 +40,15 @@
       // }
    })
    // })
+
+     // filtros nivel
+       filterExperienceLevel.addEventListener("change", function(){
+          const jobs= document.querySelectorAll(".job-listing-card");
+          const selectedValue= filterExperienceLevel.value;
+
+          jobs.forEach(job =>{
+             const nivel= job.getAttribute("data-nivel");
+             const isShown= selectedValue === "" || selectedValue === nivel;
+             job.classList.toggle("is-hidden", !isShown);
+          })
+       })
